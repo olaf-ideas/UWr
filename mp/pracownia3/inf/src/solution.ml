2e5f94dc9f1b81687c7c7db3b8423d3985009666
Olaf Surgut <olaf.ideas@gmail.com>
2024-06-08 17:04:44 +0200

    Working on all tests

--- mp/pracownia3/inf/src/solution.ml
+++ mp/pracownia3/inf/src/solution.ml
@@ -9,6 +9,8 @@ let rec print_env env =
     print_env env
   | [] -> Printf.printf ";\n"
 
+let father_dict : ((name * name) list ref) = ref []
+
 (* let rec lookup_for_variable (env : var option list) (x : var) (index : int) : (int * cmd list) =
   match env with
   | [] -> failwith ("Variable " ^ x ^ " not found")
@@ -29,13 +31,13 @@ let rec lookup_for_function_env (env : var option list) (father_name : name) (in
   Printf.printf "Father name: %s Current env: " father_name; print_env env;
   match env with
   | [] -> failwith ("Can't find function with father_name: " ^ father_name)
-  | Some y :: _ when y = ("env_pointer:" ^ father_name) -> Printf.printf "Found father_name\n"; [LOAD index]
+  | Some y :: _ when y = ("env_pointer:" ^ father_name) -> (* Printf.printf "Found father_name\n"; *) [LOAD index]
   | Some y :: env when (String.starts_with ~prefix:"env_pointer" y) ->
-    Printf.printf "Found other env_pointer: %s\n" y;
+    (* Printf.printf "Found other env_pointer: %s\n" y; *)
     LOAD index :: lookup_for_function_env env father_name 0
   | _ :: env -> lookup_for_function_env env father_name (index + 1)
 
-let rec compile_aexp (env : var option list) (father_dict : (name * name) list) (a : aexp) : cmd list =
+let rec compile_aexp (env : var option list) (dict : (name * name) list) (a : aexp) : cmd list =
   match a with
   | Int n -> [CONST n]
   | Var x ->
@@ -44,44 +46,48 @@ let rec compile_aexp (env : var option list) (father_dict : (name * name) list) 
     [TOP] @ cmds @ [LOAD idx]
   (* | Var x -> [TOP] @ lookup_for_variable env x 0 *)
   | Binop (op, a1, a2) ->
-      let c1 = compile_aexp env father_dict a1 in
-      let c2 = compile_aexp (None :: env) father_dict a2 in
+      let c1 = compile_aexp env dict a1 in
+      let c2 = compile_aexp (None :: env) dict a2 in
       c1 @ [PUSH] @ c2 @ [PRIM op]
   | Call (f, args) ->
     let rec prepare_args env args =
       match args with
       | [] -> []
-      | a :: args -> compile_aexp env father_dict a @ [PUSH] @ prepare_args (None :: env) args in
-    Printf.printf "Is there %s in father_dict?\n" f;
-    let cmds = lookup_for_function_env env (List.assoc f father_dict) 0 in
-    [TOP] @ cmds @ [PUSH] @ (prepare_args env args) @ [CALL f] @ [LEAVE (List.length args + 1)]
+      | a :: args -> compile_aexp env dict a @ [PUSH] @ prepare_args (None :: env) args in
+    (* Printf.printf "Is there %s in father_dict 1\n" f; *)
+    let g = (match List.assoc_opt f dict with
+    | Some h -> h
+    | None   -> f) in
+    (* Printf.printf "Is there %s in father_dict 2\n" f; *)
+    let cmds = lookup_for_function_env env (List.assoc g !father_dict) 0 in
+    [TOP] @ cmds @ [PUSH] @ (prepare_args (None :: env) args) @ [CALL f] @ [LEAVE (List.length args + 1)]
 
-let rec compile_bexp (env : var option list) (father_dict : (name * name) list) (b : bexp) : cmd list =
+let rec compile_bexp (env : var option list) (dict : (name * name) list) (b : bexp) : cmd list =
   match b with
   | Bool true  -> [CONST 1]
   | Bool false -> [CONST 0]
   | Cmp (op, a1, a2) ->
-    let c1 = compile_aexp env father_dict a1 in
-    let c2 = compile_aexp (None :: env) father_dict a2 in
+    let c1 = compile_aexp env dict a1 in
+    let c2 = compile_aexp (None :: env) dict a2 in
     c1 @ [PUSH] @ c2 @ [CMP op]
     (* compile_aexp env a1 @ [PUSH] @ compile_aexp (None :: env) a2 @ [CMP op] *)
   | And (b1, b2) ->
-    let c1 = compile_bexp env father_dict b1 in
-    let c2 = compile_bexp env father_dict b2 in
+    let c1 = compile_bexp env dict b1 in
+    let c2 = compile_bexp env dict b2 in
     c1 @ [BRANCH (c2, [CONST 0])]
   | Or (b1, b2) ->
-    let c1 = compile_bexp env father_dict b1 in
-    let c2 = compile_bexp env father_dict b2 in
+    let c1 = compile_bexp env dict b1 in
+    let c2 = compile_bexp env dict b2 in
     c1 @ [BRANCH ([CONST 1], c2)]
   | Not b ->
-    compile_bexp env father_dict b @ [PUSH; CONST 0; CMP Eq]
+    compile_bexp env dict b @ [PUSH; CONST 0; CMP Eq]
 
-let rec compile_stmt (env : var option list) (father_dict : (name * name) list) (s : stmt) : cmd list =
+let rec compile_stmt (env : var option list) (dict : (name * name) list) (s : stmt) : cmd list =
   match s with 
-  | Block stmts -> List.concat (List.map (compile_stmt env father_dict) stmts)
+  | Block stmts -> List.concat (List.map (compile_stmt env dict) stmts)
   | Assgn (x, a) ->
     (* Printf.printf "Assign up for variable %s in: " x; print_env env; *)
-    let ca = compile_aexp (None :: env) father_dict a in
+    let ca = compile_aexp (None :: env) dict a in
     let (idx, cmds) = lookup_for_variable_addr env x 0 in
     [TOP] @ cmds @ [PUSH] @ ca @ [STORE idx]
     (* let ca = compile_aexp (None :: env) a in
@@ -89,13 +95,13 @@ let rec compile_stmt (env : var option list) (father_dict : (name * name) list) 
     | Some i -> [TOP; PUSH] @ ca @ [STORE i]
     | None   -> failwith ("Variable " ^ x ^ " not found")) *)
   | If (b, s1, s2) ->
-    let cb = compile_bexp env father_dict b in
-    let cs1 = compile_stmt env father_dict s1 in
-    let cs2 = compile_stmt env father_dict s2 in
+    let cb = compile_bexp env dict b in
+    let cs1 = compile_stmt env dict s1 in
+    let cs2 = compile_stmt env dict s2 in
     cb @ [BRANCH (cs1, cs2)]
   | While (b, s) ->
-    let cb = compile_bexp env father_dict b in
-    let cs = compile_stmt env father_dict s in
+    let cb = compile_bexp env dict b in
+    let cs = compile_stmt env dict s in
     [WHILE (cb, cs)]
   | Read x ->
     let (idx, cmds) = lookup_for_variable_addr env x 0 in
@@ -104,9 +110,9 @@ let rec compile_stmt (env : var option list) (father_dict : (name * name) list) 
     | Some i -> [TOP; PUSH; READ; STORE i]
     | None   -> failwith ("Variable" ^ x ^ "not found")) *)
   | Write a ->
-    compile_aexp env father_dict a @ [WRITE]
+    compile_aexp env dict a @ [WRITE]
   | Return a ->
-    compile_aexp env father_dict a @ [RET]
+    compile_aexp env dict a @ [RET]
 
 let rec add_leave_at_return (local_vars : var list) (cmds : cmd list) : cmd list =
   match cmds with
@@ -148,30 +154,34 @@ let rec add_prefix_to_funs (prefix : name) (funs : func list) : (func list) =
   | Func (name, args, local_vars, local_funs, body) :: funs ->
     Func (prefix ^ name, args, local_vars, local_funs, body) :: add_prefix_to_funs prefix funs
 
-let rec compile_funs (env : var option list) (father_dict : (name * name) list) (dict : (name * name) list) (funs : func list) : (name * cmd list) list =
+let rec compile_funs (env : var option list) (dict : (name * name) list) (funs : func list) : (name * cmd list) list =
   match funs with
   | [] -> []
   | Func (name, args, local_vars, local_funs, body) :: funs ->
     let new_env = [Some ("env_pointer:" ^ name)] @ (List.map Option.some local_vars) @ [None] @ (List.rev (List.map Option.some args)) @ env in
+    Printf.printf "Function (%s) expects env:  " name; print_env new_env;
     let new_dict = dict @ (List.map (fun (Func (fun_name, _, _, _, _)) -> (fun_name, name ^ "_" ^ fun_name)) local_funs) in
     let local_funs = add_prefix_to_funs (name ^ "_") local_funs in
-    let fun_code = compile_stmt new_env father_dict body in
+    let fun_code = compile_stmt new_env new_dict body in
     let fun_code = [ENTER (List.length local_vars); TOP; PUSH] @ fun_code @ [RET] in
     let fun_code = add_leave_at_return local_vars fun_code in
     let fun_code = apply_renaming new_dict fun_code in
-    (name, fun_code) :: compile_funs new_env father_dict new_dict local_funs @ compile_funs env father_dict dict funs
+    (name, fun_code) :: compile_funs new_env new_dict local_funs @ compile_funs env dict funs
 
-let rec create_father_dict (father : name) (funs : func list) : ((name * name) list) =
+let rec create_father_dict (father : name) (funs : func list) =
   match funs with
-  | [] -> []
+  | [] -> ()
   | Func (name, _, _, local_funs, _) :: funs ->
     let local_funs = add_prefix_to_funs (name ^ "_") local_funs in
-    Printf.printf "adding to father_dict: %s -> %s\n" name father; 
-    (name, father) :: create_father_dict name local_funs @ create_father_dict father funs
+    Printf.printf "adding to father_dict: %s -> %s\n" name father;
+    father_dict := (name, father) :: !father_dict;
+    create_father_dict name local_funs;
+    create_father_dict father funs;
+    ()
 
 let compile_prog ((vars, funs, main_stmt) : prog) : vm_prog =
   let env = [Some "env_pointer:main"] @ List.map Option.some vars in
-  let father_dict = create_father_dict "main" funs in
-  let compiled_funs = compile_funs env father_dict [] funs in
-  let main_code = compile_stmt env father_dict main_stmt in
+  create_father_dict "main" funs;
+  let compiled_funs = compile_funs env [] funs in
+  let main_code = compile_stmt env [] main_stmt in
   ([ENTER (List.length vars); TOP; PUSH] @ main_code @ [LEAVE (List.length vars + 1)], compiled_funs)
